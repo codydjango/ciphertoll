b4ac0c9159aa8525b451b5efc1e8b9de7140e721
Cody Django Redmond <codydjango@gmail.com>
2019-01-16 20:38:40 -0800

    remove reference to browsersync; add express

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require('express')
-var browserSync = require('browser-sync')
 var app = express()
 var port = 3000
 
 app.listen(port, listening)
 
 function listening() {
-    console.log(`listening on localhost:${port}`)
+    console.log(`API listening on localhost:${port}`)
 }
