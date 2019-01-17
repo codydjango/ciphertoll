d1b9816d61927543b021d325d43e17ef728ddff7
Cody Django Redmond <codydjango@gmail.com>
2019-01-16 21:00:57 -0800

    add back chokidar

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-var express = require('express');
-var app = express();
-var port = 3000;
+const express = require('express');
+const app = express();
+const chokidar = require('chokidar')
 
+const watcher = chokidar.watch('./src')
+const port = 3000;
+const router = express.Router()
+
+
+watcher.on('ready', function() {
+  watcher.on('all', function() {
+    console.log("Clearing /dist/ module cache from server")
+    Object.keys(require.cache).forEach(function(id) {
+      if (/[\/\\]app[\/\\]/.test(id)) delete require.cache[id]
+    })
+  })
+})
+
+
+router.get('/hello', helloController)
 app.listen(port, listening);
+
+function helloController(req, res) {
+	res.json({ payload: 'world' })
+}
 
 function listening() {
   console.log(`API listening on localhost:${port}`);
